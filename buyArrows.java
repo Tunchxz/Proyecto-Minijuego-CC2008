b2a9a40d0cb3790fc0ad2785cb7d43dc2236db0f
JLinares162 <linareschangjavier162@gmail.com>
2023-07-28 22:51:55 -0600

    Cambios a la función comprar
    Se agregaron efectos de sonido y se realizaron cambios a la función comprar.

--- buyArrows.java
+++ buyArrows.java
@@ -12,12 +12,15 @@ public class buyArrows extends Actor
      * Act - do whatever the buyArrows wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    GreenfootSound sound = new GreenfootSound("buy.mp3");
+
     public void act() 
     {
        if (Greenfoot.isKeyDown("x") && player.money >= 10)
         {
             player.ammo = player.ammo + 5;
             player.money = player.money - 10;
+            sound.play();
         }
     }    
 }
